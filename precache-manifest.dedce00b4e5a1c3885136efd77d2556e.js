de4ebcebc6e6900219167b9a9f4dc99a11e8ad58
Ishani Dabral (Gurugram -90A - Information Technology) <ishani.dabral1@maxlifeinsurance.com>
2020-08-11 12:49:52 +0530

    Updates

---
+++ precache-manifest.dedce00b4e5a1c3885136efd77d2556e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ede225d3c8a287453b48a619875d21",
+    "url": "/Organizer-app/index.html"
+  },
+  {
+    "revision": "5eb242291bc81cc5fd00",
+    "url": "/Organizer-app/static/css/main.045e1d8a.chunk.css"
+  },
+  {
+    "revision": "44ba9fcbaf43448f09e5",
+    "url": "/Organizer-app/static/js/2.64a1dc65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Organizer-app/static/js/2.64a1dc65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eb242291bc81cc5fd00",
+    "url": "/Organizer-app/static/js/main.e6692230.chunk.js"
+  },
+  {
+    "revision": "8c9efe512c2afd9b96a8",
+    "url": "/Organizer-app/static/js/runtime-main.8ceebc79.js"
+  }
+]);
